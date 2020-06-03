d4312545231c3c2d47337587113773035f6bde40
YB <alex56599@yandex.ru>
2020-06-03 09:30:41 +0300

    Finish 1st step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,4 @@ import program from 'commander';
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information');
+    .parse(process.argv);
